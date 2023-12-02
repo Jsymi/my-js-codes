cf26f76e7bd456a5117e50435ebba17649b63df2
Jsymi <143898571+Jsymi@users.noreply.github.com>
2023-12-02 03:36:09 +0000

    nums and math

---
+++ 01basic-js-code/nums_and_math.js
@@ -0,0 +1,15 @@
+const score = 400
+// console.log(score);
+// console.log(typeof score);
+
+const balance = new Number(100)
+console.log(balance);    //[Number: 100] - Here we can perform soem operation.
+
+console.log(balance.toString());
+console.log(balance.toFixed(2)); 
+
+const otherNum = 124.7865
+console.log(otherNum.toPrecision(3));
+
+const hundred = 100000000
+console.log(hundred.toLocaleString('en-IN'));
